3611a5b03c9c95fc7498c7b38e70698340baed41
Vovun <mrvovun@gmail.com>
2018-11-12 09:00:02 +0200

    Player movement, camera follow, added enemy

---
+++ Assets/Scripts/EnemyInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInteraction : MonoBehaviour {
+
+	public float pushForce = 3f;
+
+	private void OnCollisionEnter (Collision other) {
+		if (other.gameObject.tag == "Enemy") {
+			Debug.Log ("Ti pidor");
+		}
+	}
+
+}
